6332d08d98b4ddfc5942b762f79b14b4262ac09d
Vikram Rangaraj <vik120@icloud.com>
2018-10-13 21:07:30 -0700

    Clean up jest configuration to avoid restating babel configs (#98)
    * use babelrcRoots in babel.config.js to avoid combined babel config
    
    * attempt to fix travis by enforcing yarn latest version
    
    * change netlify script to use latest yarn version
    
    * use workspaces.packages or workspaces
    
    * use eslintignore for prettier, add stop script to root
    
    * use prod env when building backend, add test script to backend
    
    * add postinstall script to frontend
    
    * add debug option to babel preset env for frontend

--- babel.config.js
+++ babel.config.js
@@ -5,30 +5,8 @@
   import/no-extraneous-dependencies: 0 
 */
 
-'use strict';
+const { workspaces = [] } = require('./package.json');
 
-// {
-//   "presets": ["@babel/preset-env", "@babel/preset-flow", "@babel/preset-react"]
-//   // "plugins": [
-//   //   "dynamic-import-node"
-//   // ]
-// }
-
-module.exports = api => {
-  if (!api.env('test')) {
-    return {};
-  }
-
-  return {
-    presets: [
-      [
-        require('@babel/preset-env'),
-        { targets: { node: 10 }, useBuiltIns: 'usage' },
-      ],
-      require('@babel/preset-flow'),
-      [require('@babel/preset-react'), { development: true }],
-    ],
-
-    plugins: [require('babel-plugin-dynamic-import-node')],
-  };
+module.exports = {
+  babelrcRoots: workspaces.packages || workspaces,
 };
