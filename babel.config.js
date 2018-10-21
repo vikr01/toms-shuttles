df018e7e1b1c6605b3919f0984b50169419040ac
Vikram Rangaraj <vik120@icloud.com>
2018-10-21 12:47:21 -0700

    clean up linting in root (#101)

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
-/* eslint
-  strict: 0, 
-  import/no-commonjs: 0, 
-  global-require: 0, 
-  import/no-extraneous-dependencies: 0 
-*/
-
 const { workspaces = [] } = require('./package.json');
 
 module.exports = {
